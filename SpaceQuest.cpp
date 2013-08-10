8c39017e81f3b4af443e9365679a8d0cff6e9b65
Programming <Programming@lab12.flpl.local>
2013-08-10 12:01:15 -0500

    Developed more story. Began righting branching functions.

--- SpaceQuest.cpp
+++ SpaceQuest.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
 
+void broughtIntoShip();
+void getHelmet();
+void getSpacePony();
+
 int main(){
     char repeat;
     do{
@@ -27,12 +31,38 @@ int main(){
         }else{
             cout<<"You stupid or somethin'?! That is definitely not an answer..."<<endl<<"You know what?! You're done. Game over..."<<endl<<endl<<"..."<<endl<<"...Jerk......."<<endl;
         }
-        cout<<"Press enter to continue...\n";
-        cin.clear();
-        cin.get();
-//        system("clear");
+        cout<<"..."<<endl<<"..."<<endl<<"..."<<endl;
+        int choice=0;
+        cout<<"Okay, fine. I guess I'll help you."<<endl;
+        cout<<"Which would you like?"<<endl;
+        cout<<"1. For us to bring you into our ship."<<endl;
+        cout<<"2. A helmet."<<endl;
+        cout<<"3. A majestic, air-producing, space pony."<<endl;
+        cin>>choice;
+        switch(choice){
+            case 1:
+                broughtIntoShip(); break;
+            case 2:
+                getHelmet(); break;
+            case 3:
+                getSpacePony(); break;
+            default:
+                cout<<"Alright, fine! You wanna play that way?! You can just stay out there. I tried! Good luck with the head exploding... Jerk..."<<endl;
+        }
         cout<<"Would you like to play again? (y/n)"<<endl;
         cin>>repeat;
     }while(repeat=='y' || repeat=='Y');
 	return 0;
+}
+
+void broughtIntoShip(){
+    cout<<"You made it into the ship..."<<endl;
+}
+
+void getHelmet(){
+    cout<<"You get a helmet...\n But you're still floating in space...\n Things don't look great for you..."<<endl;
+}
+
+void getSpacePony(){
+    cout<<"You got a freakin' awesome, air-producing, moss-covered, three-handled, family space pony...\n Looks like you're set."<<endl;
 }
